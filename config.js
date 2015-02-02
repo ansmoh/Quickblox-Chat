aa59042a6ebc62790b2ba3af39e68d0f205b3c3b
ansmoh <anshuman.ewest@gmail.com>
2015-02-03 04:48:08 +0530

    Update
    Update

---
+++ config.js
@@ -0,0 +1,8 @@
+/* Insert your QB application credentials here
+-----------------------------------------------*/
+var QBAPP = {
+	appID: 8445,
+	authKey: 'yyRRFJMjRqYVdgT',
+	authSecret: 'zRmYWAr5djh48fu',
+	publicRoom: 'public'
+};
